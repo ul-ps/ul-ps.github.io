bd46b476efbc803fcff9e608d55017d6aa680c4e
ul-ps <uljewads@gmail.com>
2026-04-24 00:24:36 +0300

    Add files via upload

---
+++ u/firebase-config.js
@@ -0,0 +1,11 @@
+
+// Firebase Configuration provided by user
+export const firebaseConfig = {
+  apiKey: "AIzaSyDi-ISQ_83WP6cQAFSpjlsxmqdV6i0Bel4",
+  authDomain: "ulps-16b02.firebaseapp.com",
+  projectId: "ulps-16b02",
+  storageBucket: "ulps-16b02.firebasestorage.app",
+  messagingSenderId: "28882791693",
+  appId: "1:28882791693:web:1a7b9fa1bfedd24bc25444",
+  measurementId: "G-HJZYD27XR6"
+};
