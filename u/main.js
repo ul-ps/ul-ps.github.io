fde0cde8bc1e34353731fe41c28a168f54efa4af
ul-ps <uljewads@gmail.com>
2026-04-24 20:36:52 +0300

    Add files via upload

---
+++ u/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        icon: path.join(__dirname, 'icon.png') // Optional, if you have one
+    });
+
+    win.loadFile('index.html');
+
+    // Remove the default menu bar (File, Edit, etc.)
+    win.removeMenu();
+
+    // win.webContents.openDevTools(); // Uncomment to debug
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
